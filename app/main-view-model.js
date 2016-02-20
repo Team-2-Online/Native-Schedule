a170823227bbf1335969d73290a4a2832b2f97a5
V3ronique <veronica.velkova@gmail.com>
2016-02-20 20:44:37 +0200

    Created notes-page.js, notes-page.xml, notes-view-model.js, added event on "GO TO NOTES" button

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,12 +1,15 @@
-var observable = require("data/observable");
-var HelloWorldModel = (function (_super) {
-    __extends(HelloWorldModel, _super);
-    function HelloWorldModel() {
+'use strict'
+let observable = require("data/observable");
+let frameModule = require('ui/frame');
+
+var HomePageModel = (function (_super) {
+    __extends(HomePageModel, _super);
+    function HomePageModel() {
         _super.call(this);
         this.counter = 42;
         this.set("message", this.counter + " taps left");
     }
-    HelloWorldModel.prototype.tapAction = function () {
+    HomePageModel.prototype.tapAction = function () {
         this.counter--;
         if (this.counter <= 0) {
             this.set("message", "Hoorraaay! You unlocked the NativeScript clicker achievement!");
@@ -15,7 +18,20 @@ var HelloWorldModel = (function (_super) {
             this.set("message", this.counter + " taps left");
         }
     };
-    return HelloWorldModel;
+    
+    HomePageModel.prototype.navigateToNotesPage = function(args) {
+        var navigationEntry = {
+				moduleName: "./notes/notes-page",
+				backstackVisible: true,
+                animated: true,
+                navigationTransition: {
+                    transition: "curl"
+                },
+			};
+
+			frameModule.topmost().navigate(navigationEntry);
+    };
+    return HomePageModel;
 })(observable.Observable);
-exports.HelloWorldModel = HelloWorldModel;
-exports.mainViewModel = new HelloWorldModel();
+exports.HomePageModel = HomePageModel;
+exports.mainViewModel = new HomePageModel();
---
+++ app/notes/notes-page.js
@@ -0,0 +1,8 @@
+'use strict'
+var vmModule = require("./notes-view-model");
+function pageLoaded(args) {
+    //var page = args.object;
+    //page.bindingContext = vmModule.notesViewModel;
+}
+
+exports.pageLoaded = pageLoaded;
