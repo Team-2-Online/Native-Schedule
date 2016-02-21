9735b8af31aefd8c36b58110c3bf5daa31462010
Pavel <pawel.angelow@gmail.com>
2016-02-21 13:38:41 +0200

    Added usage of the navigation for navigating all notes and events

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -4,40 +4,32 @@ let frameModule = require('ui/frame');
 //Example use of DB
 var liteDb = require(__base + "common/SQLiteDatabase");
 var exampleModel = require(__base + "data-models/example");
+var navigation = require(__base + "common/navigation");
 
 var HomePageModel = (function (_super) {
-    __extends(HomePageModel, _super);
-    function HomePageModel() {
-        _super.call(this);
-        this.counter = 42;
-        this.set("message", this.counter + " taps left");
-    }
-    HomePageModel.prototype.tapAction = function () {
-      //Example use of db
-      liteDb.createTable(exampleModel);
-      //End of example use
-        this.counter--;
-        if (this.counter <= 0) {
-            this.set("message", "Hoorraaay! You unlocked the NativeScript clicker achievement!");
-        }
-        else {
-            this.set("message", this.counter + " taps left");
-        }
-    };
-    
-    HomePageModel.prototype.navigateToNotesPage = function(args) {
-        var navigationEntry = {
-				moduleName: "./notes/notes-page",
-				backstackVisible: true,
-                animated: true,
-                navigationTransition: {
-                    transition: "curl"
-                },
-			};
+	__extends(HomePageModel, _super);
+	function HomePageModel() {
+		_super.call(this);
+		this.counter = 42;
+		this.set("message", this.counter + " taps left");
+	}
+	HomePageModel.prototype.tapAction = function () {
+		//Example use of db
+		liteDb.createTable(exampleModel);
+		//End of example use
+		this.counter--;
+		if (this.counter <= 0) {
+			this.set("message", "Hoorraaay! You unlocked the NativeScript clicker achievement!");
+		}
+		else {
+			this.set("message", this.counter + " taps left");
+		}
+	};
 
-			frameModule.topmost().navigate(navigationEntry);
-    };
-    return HomePageModel;
+	HomePageModel.prototype.navigateToNotesPage = navigation.goToAllNotes;
+
+	HomePageModel.prototype.navigateToEventsPage = navigation.goToAllEvents;
+
+	return HomePageModel;
 })(observable.Observable);
-exports.HomePageModel = HomePageModel;
 exports.mainViewModel = new HomePageModel();
