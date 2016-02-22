c0ab072ac44894d3f4975d76c7262e24a3f3e7f6
V3ronique <veronica.velkova@gmail.com>
2016-02-22 19:57:19 +0200

    added geolocation

--- app/views/events/create-event/create-event.js
+++ app/views/events/create-event/create-event.js
@@ -11,6 +11,69 @@ var liteDb = require(__base + "common/SQLiteDatabase");
 var navigation = require(__base + "common/navigation");
 var validator = require("../../../common/validation");
 var Toast = require('nativescript-toast');
+var mapsModule = require("nativescript-google-maps-sdk");
+var geolocation = require("nativescript-geolocation");
+
+var topmost;
+var camera;
+var mapView;
+var myLatitude;
+var myLongitude;
+
+function onMapReady(args) {
+
+    var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10}).
+        then(function(loc) {
+            if (loc) {
+                myLatitude = loc.longitude;
+                myLongitude = loc.latitude;
+
+                mapView = args.object;
+
+                var marker = new mapsModule.Marker();
+                marker.position = mapsModule.Position.positionFromLatLng(myLongitude,myLatitude);
+                marker.title = "Party";
+                marker.snippet = "Here";
+                marker.userData = { index : 1};
+                mapView.addMarker(marker);
+            }
+        }, function(e){
+            console.log("Error: " + e.message);
+        });
+}
+
+function onMarkerSelect(args) {
+    console.log("Clicked on " +args.marker.title);
+}
+
+function onCameraChanged(args) {
+
+    console.log("Camera changed: " + JSON.stringify(args.camera));
+}
+
+function onCoordinateTapped(args){
+    console.log("Clicked ggon " +args.coords);
+}
+
+function pageLoaded(args) {
+    topmost = frameModule.topmost();
+
+}
+function addLocation(eventData) {
+
+   globalConstants.Latitude = myLatitude;
+   globalConstants.Longitude = myLongitude;
+
+    console.log(globalConstants.Latitude);
+    console.log(globalConstants.Longitude);
+    topmost.goBack();
+}
+exports.addLocation = addLocation;
+exports.pageLoaded = pageLoaded;
+exports.onMapReady = onMapReady;
+exports.onMarkerSelect = onMarkerSelect;
+exports.onCameraChanged = onCameraChanged;
+exports.onCoordinateTapped = onCoordinateTapped;
 
 var pageModules = (function() {
 
